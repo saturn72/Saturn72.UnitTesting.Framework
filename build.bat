d5c728e4fceb3f84b2898a04c1f387d6151d5650
roi@saturn72.com <roi@saturn72.com>
2016-08-08 15:56:54 +0300

    fix build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @echo Off
 
-set slnDir="Saturn72.UnitTesting.Framework"
-set slnName="Saturn72.UnitTesting.Framework.sln"
-set prjName="Saturn72.UnitTesting.Framework.csproj"
-
+set slnDir=Saturn72.UnitTesting.Framework
+set slnName=%slnDir%\Saturn72.UnitTesting.Framework.sln
+set prjName=%slnDir%\Saturn72.UnitTesting.Framework.csproj
+ 
 set config=%1
 if "%config%" == "" (
    set config=Release
@@ -18,10 +18,10 @@ REM Restore nuget packages
 %NuGet% restore -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
-echo Start building %slnDir%\%slnName%
-"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" %slnDir%\%slnName% /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+echo Start building %slnName%
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" %slnName% /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir Build
-echo Packging %slnDir%"\"%prjName% project to Build directory
-call %nuget% pack %slnDir%"\"%prjName% -symbols -o Build -p Configuration=%config% %version%
+echo Packging %prjName% project to Build directory
+call %nuget% pack %prjName% -symbols -o Build -p Configuration=%config% %version%
